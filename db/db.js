46fa368cb363b27575b96a02a24e76fa920ac7dc
Eduard Voicu <iameduardvoicu@gmail.com>
2021-10-19 21:58:17 -0500

    added to function on db file

--- db/db.js
+++ db/db.js
@@ -15,3 +15,22 @@ const db = mysql.createConnection({
     password: connectionInfo.db_pass,
     database: "employeeTrackerDB"
 });
+
+// Make the connection
+db.connect(err => {
+    if (err) throw err;
+    // console.log("connected as id "+connection.threadId);
+    app.init();
+});
+
+var showAll = (table_name,callback) => {
+    let query = "";
+    if (table_name === "employees") {
+        // Show all employees first name, last name, role, salary, department, and manager name
+        query = `SELECT emp1.firstName AS 'First Name', emp1.lastName AS 'Last Name', title AS 'Title', name AS 'Department', salary AS 'Salary', GROUP_CONCAT(DISTINCT emp2.firstName,' ', emp2.lastName) AS 'Manager'
+        FROM employees emp1
+        JOIN roles ON emp1.role_id = roles.id
+        JOIN departments ON roles.department_id = departments.id
+        LEFT JOIN employees emp2 ON emp1.manager_id = emp2.id
+        GROUP BY emp1.id
+        ORDER BY emp1.lastName ASC`;
